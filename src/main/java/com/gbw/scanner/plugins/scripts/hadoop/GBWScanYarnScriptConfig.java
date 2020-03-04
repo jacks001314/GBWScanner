ed58c263e7c6be9b8ae25b206e2f55d6d99e857e
Your Name <you@example.com>
2020-03-04 16:21:49 +0800

    add

--- src/main/java/com/gbw/scanner/plugins/scripts/hadoop/GBWScanYarnScriptConfig.java
+++
@@ -1,7 +0,0 @@
-package com.gbw.scanner.plugins.scripts.hadoop;
-
-import com.gbw.scanner.plugins.scripts.GBWScanScriptCommonConfig;
-
-public class GBWScanYarnScriptConfig extends GBWScanScriptCommonConfig {
-
-}
